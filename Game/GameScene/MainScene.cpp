37a25d6596745cf5cf1c3df93958f62cc2623a83
Shimamoto Hisanori <ca01254009@st.kawahara.ac.jp>
2026-08-24 11:01:10 +0900

    メイン画面を実装

---
+++ Game/GameScene/MainScene.cpp
@@ -0,0 +1,39 @@
+#include "stdafx.h"
+#include "MainScene.h"
+namespace
+{
+	/** MainSceneの画像のファイルパス */
+	const char* MAIN_SCENE_IMAGE_FILE_PATH = "Assets/sprites/MainScene.dds";
+
+	/** MainSceneの画像の横幅 */
+	const float MAIN_SCENE_IMAGE_WIDTH = 1920.0f;
+
+	/** MainSceneの画像の縦幅 */
+	const float MAIN_SCENE_IMAGE_HEIGHT = 1080.0f;
+
+	/** MainSceneの画像位置 */
+	const Vector3 MAIN_SCENE_IMAGE_POSITION = Vector3(0.0f, 0.0f, 0.0f);
+}
+
+MainScene::MainScene()
+{}
+
+MainScene::~MainScene()
+{}
+
+bool MainScene::Start()
+{
+	m_mainSpriteRender.Init(MAIN_SCENE_IMAGE_FILE_PATH, MAIN_SCENE_IMAGE_WIDTH, MAIN_SCENE_IMAGE_HEIGHT);
+	m_mainSpriteRender.SetPosition(MAIN_SCENE_IMAGE_POSITION);
+	return true;
+}
+
+void MainScene::Update()
+{
+	m_mainSpriteRender.Update();
+}
+
+void MainScene::Render(RenderContext & rc)
+{
+	m_mainSpriteRender.Draw(rc);
+}
---
+++ Game/GameScene/MainScene.h
@@ -0,0 +1,18 @@
+#pragma once
+
+/** ゲーム画面クラス */
+class MainScene : public IGameObject
+{
+public:
+	MainScene();
+	~MainScene();
+
+	bool Start() override;
+	void Update() override;
+	void Render(RenderContext& rc) override;
+
+private:
+	/** メイン画面の画像 */
+	SpriteRender m_mainSpriteRender;
+};
+
